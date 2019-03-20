84818dba2fefb4851b1e1a266b9af2f0311a5b60
Adrien Baudet <baudetad@gmail.com>
2019-03-20 07:48:45 +0100

    fait de l'exception une runtimeException

--- MessageExeption.java
+++ MessageExeption.java
@@ -5,7 +5,7 @@
  * @author Hellper (Adrien Baudet)
  * @version 01/03/2018
  */
-public class MessageExeption extends Exception
+public class MessageExeption extends RuntimeException
 {
     public MessageExeption(String message){
         super(message);
